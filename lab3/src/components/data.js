99f901faaf18c4f1d557e37a55033fd950982ce2
binhbinh214 <binhhu21974@gmail.com>
2025-02-07 14:32:26 +0700

    lab3

---
+++ lab3/src/components/data.js
@@ -0,0 +1,18 @@
+export const data = [
+  {
+    question: "What is the capital of France?",
+    option1: "Paris",
+    option2: "London",
+    option3: "Berlin",
+    option4: "Madrid",
+    ans: 1,
+  },
+  {
+    question: "What is the largest planet in our solar system?",
+    option1: "Jupiter",
+    option2: "Saturn",
+    option3: "Mars",
+    option4: "Earth",
+    ans: 1,
+  },
+];
