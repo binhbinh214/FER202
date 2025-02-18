d7348a1c6d9bd806f58cd115e8b47a13cd3cf1ac
binhbinh214 <binhhu21974@gmail.com>
2025-02-18 22:12:36 +0700

    add

--- lab4/src/App.js
+++
@@ -1,8 +0,0 @@
-import Quiz from "./Components/Quiz.js";
-import { jsQuizz } from "./Components/Data.js";
-
-function App() {
-  return <Quiz questions={jsQuizz.questions} />;
-}
-
-export default App;
